fe8e673876d24b33fccff4ded806d1f291f12181
claire <claire@barkingdog.ai>
2023-11-27 13:34:43 +0800

    feat: 新增lambda 存取s3 bucket功能

--- lambda_function.py
+++ lambda_function.py
@@ -1,26 +1,43 @@
 
+import logging
 import argparse
 from book_maker.cli import translate
+import boto3
+import os
+s3 = boto3.client('s3')
 
 
 def handler(event, context):
-    options = argparse.Namespace(
-        book_name=event['book_name'],
-        language=event['language'],
-        openai_key=event['openai_key'],
-        api_base=event['api_base'],
-        deployment_id=event['deployment_id'],
-        folder_path=event['folder_path'],
-        prompt=event['prompt'],
-    )
+    logger = logging.getLogger()
+    logger.setLevel(logging.INFO)
+    records = event.get('Records', [])
 
-    translate(options)
+    for record in records:
+        bucket = record['s3']['bucket']['name']
+        key = record['s3']['object']['key']
+
+        #  Lambda /tmp 目錄下的文件路徑
+        download_path = '/tmp/{}'.format(os.path.basename(key))
+        s3.download_file(bucket, key, download_path)
+        logger.info(f"download_path: {download_path}")
+        options = argparse.Namespace(
+            file_path=download_path,
+            language=os.environ['language'],
+            openai_key=os.environ['openai_key'],
+            api_base=os.environ['api_base'],
+            deployment_id=os.environ['deployment_id'],
+            folder_path=os.environ['folder_path'],
+            prompt=os.environ['prompt'],
+        )
+
+        options.file_path = download_path
+
+        # translate(options)
+
+        os.remove(download_path)
+        logger.info(f"remove file: {download_path}")
+
     return {
-        'book_name': event['book_name'],
-        'language': event['language'],
-        'openai_key': event['openai_key'],
-        'api_base': event['api_base'],
-        'deployment_id': event['deployment_id'],
-        'folder_path': event['folder_path'],
-        'prompt': event['prompt'],
+        'statusCode': 200,
+        'body': 'Lambda function executed successfully.'
     }
